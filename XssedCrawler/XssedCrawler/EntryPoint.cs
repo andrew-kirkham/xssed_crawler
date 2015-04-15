9a299c23d6d768fc6104dfc450a8f7f3120a033b
andrew-kirkham <andrew.kirkham@marquette.edu>
2015-04-15 10:49:35 -0500

    added prompt on startup

--- XssedCrawler/XssedCrawler/EntryPoint.cs
+++ XssedCrawler/XssedCrawler/EntryPoint.cs
@@ -11,9 +11,35 @@ namespace XssedCrawler {
 	class EntryPoint {
 
 		static void Main(string[] args) {
-			//Crawler c = new Crawler();
-			AsyncCrawler c = new AsyncCrawler();
-			c.Crawl(@"http://www.dmoz.org/");
+			FileManager.WriteClassifiedToArff(null);
+			char selection = promptUser();
+			selectTask(selection);
+		}
+
+		private static void selectTask(char selection) {
+			switch (selection) {
+				case 'c':
+					var c = new AsyncCrawler();
+					c.Crawl(@"http://www.dmoz.org/");
+					break;
+				case 'x':
+					var x = new XssCrawler();
+					x.Crawl();
+					break;
+				case 'm':
+					var m = new Classification();
+					m.ClassifyAll();
+					break;
+				default:
+					Console.WriteLine("Command not recognized");
+					selectTask(promptUser());
+					break;
+			}
+		}
+
+		private static char promptUser() {
+			Console.WriteLine("Input Command: (c) crawl all websites (x) save xssed data (m) classify data");
+			return (char)Console.Read();
 		}
 	}
 }
